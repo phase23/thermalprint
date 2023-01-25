58ff5e1bc339cde7b4e7cb861ed0cad2dc6dc40f
Axcess <con3ro11@gmail.com>
2023-01-25 01:09:04 -0400

    mycom

--- app/src/main/java/com/dantsu/thermalprinter/MyService.java
+++ app/src/main/java/com/dantsu/thermalprinter/MyService.java
@@ -68,9 +68,23 @@ public class MyService extends Service {
     public void onCreate() {
 
         Intent notificationIntent = new Intent(this, MainActivity.class);
-
+/*
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
                 notificationIntent, 0);
+
+
+ */
+
+        PendingIntent pendingIntent = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+            pendingIntent = PendingIntent.getActivity
+                    (this, 0, notificationIntent, PendingIntent.FLAG_MUTABLE);
+        }
+        else
+        {
+            pendingIntent = PendingIntent.getActivity
+                    (this, 0, notificationIntent, PendingIntent.FLAG_ONE_SHOT);
+        }
 
         Notification notification = new NotificationCompat.Builder(this)
                 //.setSmallIcon(R.mipmap.app_icon)
